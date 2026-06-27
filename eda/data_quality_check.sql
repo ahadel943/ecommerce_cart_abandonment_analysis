5c29308bd84cfd36e447fffbc78dfad41a86cde2
ahadel943 <ahadel943@gmail.com>
2026-06-27 03:09:55 +0300

    DATA QUALITY CHECK: missing values check

--- eda/data_quality_check.sql
+++ eda/data_quality_check.sql
@@ -8,7 +8,9 @@ select * from raw_data.orders limit 10;
 select * from raw_data.cart_events limit 10;
 select * from raw_data.abandonment_reasons limit 10;
 
--- duplicated rows check
+-- ===============================================
+-- 							duplicated rows check
+-- ===============================================
 -- users table duplicates check
 select 
 	count(*) as duplicates_count
@@ -110,22 +112,89 @@ group by
 	confidence
 having count(*) > 1; -- NO duplicates found
 	
+-- ===============================================
+-- 							missing values check
+-- ===============================================
+-- users table missing values check
+select
+	count(*) filter(where user_id is null) as user_id_missing, -- NO missing values
+	count(*) filter(where signup_date is null) as signup_date_missing, -- NO missing values
+	count(*) filter(where country is null) as country_missing, -- NO missing values
+	count(*) filter(where city is null) as city_missing, -- 5,125 missing values in the city column, 5.12% of the total volume
+	count(*) filter(where device is null) as device_missing, -- NO missing values
+	count(*) filter(where acquisition_channel is null) as acquisition_channel_missing, -- NO missing values
+	count(*) filter(where customer_type is null) as customer_type_missing, -- NO missing values
+	count(*) filter(where age is null) as age_missing, -- NO missing values
+	count(*) filter(where is_premium is null) as is_premium_missing -- NO missing values
+from raw_data.users;
 	
+select 
+	count(*) filter(where city is null) * 1.0 * 100.0 / count(*) as perc
+from raw_data.users;
+
+-- products table missing values check
+select
+	count(*) filter(where product_id is null) as product_id_missing, -- NO missing values
+	count(*) filter(where product_name is null) as product_name_missing, -- NO missing values
+	count(*) filter(where category is null) as category_missing, -- NO missing values
+	count(*) filter(where brand is null) as brand_missing, -- NO missing values
+	count(*) filter(where price is null) as price_missing, -- NO missing values
+	count(*) filter(where cost is null) as cost_missing, -- NO missing values
+	count(*) filter(where rating is null) as rating_missing, -- NO missing values
+	count(*) filter(where stock is null) as stock_missing, -- NO missing values
+	count(*) filter(where is_active is null) as is_active_missing -- NO missing values
+from raw_data.products;
+
+-- orders table missing values check
+select 
+	count(*) filter(where order_id is null) as order_id_missing, -- NO missing values
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where total_amount is null) as total_amount_missing, -- NO missing values
+	count(*) filter(where payment_method is null) as payment_method_missing -- NO missing values
+from raw_data.orders;
 	
-	
-	
-	
-	
-	
+-- checkout_attempts table missing values check
+select 
+	count(*) filter(where checkout_id is null) as checkout_id_missing, -- NO missing values
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where started_at is null) as started_at_missing, -- NO missing values
+	count(*) filter(where payment_method is null) as payment_method_missing, -- NO missing values
+	count(*) filter(where shipping_cost is null) as shipping_cost_missing, -- NO missing values
+	count(*) filter(where completed is null) as completed_missing -- NO missing values
+from raw_data.checkout_attempts;
 
+-- carts table missing values check
+select
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where user_id is null) as user_id_missing, -- NO missing values
+	count(*) filter(where created_at is null) as created_at_missing, -- NO missing values
+	count(*) filter(where status is null) as status_missing -- NO missing values
+from raw_data.carts;
 
+-- cart_items table missing values check
+select
+	count(*) filter(where cart_item_id is null) as cart_item_id_missing, -- NO missing values
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where product_id is null) as product_imissing, -- NO missing values
+	count(*) filter(where quantity is null) as quantity_missing, -- NO missing values
+	count(*) filter(where unit_price is null) as unit_price_missing -- NO missing values
+from raw_data.cart_items;
 
+-- cart_events table missing values check
+select
+	count(*) filter(where event_id is null) as event_id_missing, -- NO missing values
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where user_id is null) as user_id_missing, -- NO missing values
+	count(*) filter(where event_name is null) as event_name_missing, -- NO missing values
+	count(*) filter(where event_time is null) as event_time_missing -- NO missing values
+from raw_data.cart_events;
 
-
-
-
-
-
+-- abandonment_reasons table missing values check
+select
+	count(*) filter(where cart_id is null) as cart_id_missing, -- NO missing values
+	count(*) filter(where reason is null) as reason_missing, -- NO missing values
+	count(*) filter(where confidence is null) as confidence_missing -- NO missing values
+from raw_data.abandonment_reasons;
 
 
 
