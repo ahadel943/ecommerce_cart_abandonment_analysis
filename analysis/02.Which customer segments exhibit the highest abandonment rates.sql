9e5b392ca7d7331e5d08f63ce91e0008780582e1
ahadel943 <ahadel943@gmail.com>
2026-07-22 03:40:21 +0300

    ANALYSIS: 2.3 - CAR by Age Group

--- analysis/02.Which customer segments exhibit the highest abandonment rates.sql
+++ analysis/02.Which customer segments exhibit the highest abandonment rates.sql
@@ -66,7 +66,7 @@ from (
 	order by "Abandoned Carts Count" desc
 ) as d;
 
--- 2.3 - CAR by Premium Status
+-- 2.3 - CAR by premium status
 with carts_info as (
 	select 
 		c.cart_id as "Cart ID",
@@ -101,6 +101,64 @@ from (
 	group by ci."Premium Status"
 	order by "Abandoned Carts Count" desc
 ) as d;
+
+-- 2.3 - CAR by age group
+with carts_info as (
+	select 
+		c.cart_id as "Cart ID",
+		case 
+			when u.age between 18 and 24 then 'Young Adults'
+			when u.age between 25 and 34 then 'Adults'
+			when u.age between 35 and 44 then 'Mid-age Adults'
+			when u.age between 45 and 54 then 'Mature Adults'
+			else 'Seniors'
+		end as "Age Group",
+		count(ci.cart_item_id) as "Items Count",
+		max(
+			case
+				when o.order_id is not null then 1
+				else 0
+			end
+		) as "Has Order"
+	from analytics_data.carts as c
+	left join analytics_data.cart_items as ci
+	on c.cart_id = ci.cart_id
+	left join analytics_data.orders as o
+	on c.cart_id = o.cart_id
+	left join analytics_data.users as u
+	on c.user_id = u.user_id 
+	group by c.cart_id, "Age Group"
+)
+select
+	d."Age Group",
+	d."Eligible Carts Count",
+	d."Abandoned Carts Count",
+	round((1.0 * d."Abandoned Carts Count") / (1.0 * d."Eligible Carts Count"), 4) as "CAR"
+from (
+	select
+		ci."Age Group",
+		count(*) filter(where ci."Items Count" >= 1) as "Eligible Carts Count",
+		count(*) filter(where ci."Items Count" >= 1 and ci."Has Order" = 0) as "Abandoned Carts Count"
+	from carts_info as ci
+	group by ci."Age Group"
+	order by "Abandoned Carts Count" desc
+) as d;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
